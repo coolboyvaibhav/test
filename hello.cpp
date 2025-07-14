27fbabd0dd3a65dbe763c8791fbbe89500c1e6ae
Vaib <nobita.hack2209@gmail.com>
2025-07-15 00:02:06 +0530

    chnage

--- hello.cpp
+++ hello.cpp
@@ -3,10 +3,6 @@ using namespace std;
 
 int main()
 {
-    cout<<"LOGO "<<endl;    
-    cout<<"LOGO2 "<<endl;    
-    cout<<"LOGO2 "<<endl;    
-    cout<<"hello world"<<endl;
-    cout<<"hello world"<<endl;
+    cout<<"changes";
     return 0;
 }
